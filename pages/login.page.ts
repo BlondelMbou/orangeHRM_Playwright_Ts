278eda039e43bd4446de90fb60a10638701011c8
BlondelMbou <blondel.mbou@gmail.com>
2024-11-28 16:05:09 +0100

    implementation de la page login

---
+++ pages/login.page.ts
@@ -0,0 +1,34 @@
+import { Page } from '@playwright/test';
+
+class LoginPage {
+    private page: Page;
+
+    constructor(page: Page) {
+        this.page = page;
+    }
+    elements = {
+        usernameLocator: () => this.page.locator('input[name="username"]'),
+        passwordLocator: () => this.page.locator('input[name="password"]'),
+        submitButtonLocator: () => this.page.locator('button[type="submit"]'),
+    }
+    async visit(url: string) {
+        await this.page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
+    async enterUsername(username: string) {
+        await this.elements.usernameLocator().fill(username);
+    }
+    async enterPassword(password: string) {
+        await this.elements.passwordLocator().fill(password)
+    }
+    async submit() {
+        await this.elements.submitButtonLocator().click();
+    }
+    async login(username: string, password: string, url: string) {
+        await this.visit(url);
+        await this.enterUsername(username);
+        await this.enterPassword(password);
+        await this.submit();
+    }
+}
+
+export default LoginPage;
