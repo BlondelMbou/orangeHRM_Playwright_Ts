77d190330c2b4eeb09253f5febf1843f98a64eaf
ayman hassnaoui <ayman12hassnaoui@gmail.com>
2024-12-01 18:46:42 +0100

    ajouter le test  de recherche button

--- pages/recherche.page.ts
+++ pages/recherche.page.ts
@@ -1,4 +1,4 @@
-import { type Page } from "@playwright/test";
+import { expect, type Page } from "@playwright/test";
 
 export class SearchPage {
     readonly page: Page;
@@ -8,13 +8,26 @@ export class SearchPage {
     }
 
     elements = {
-       
+
         searchButton: () => this.page.locator('input.oxd-input.oxd-input--active[placeholder="Search"]'),
+        searchInput: () => this.page.locator('input.oxd-input.oxd-input--focus[placeholder="Search"]'),
+        resultsList: () => this.page.locator('ul.oxd-main-menu'),
+        listItems: () => this.page.locator('ul.oxd-main-menu li'),
     };
 
-    async clicksearchButton() {
-        await this.elements.searchButton().click();
+   
+
+    async clickInputAndFill(value: string) {
+        const searchButton = this.elements.searchButton();
+        await searchButton.click();
+        const searchInput = this.elements.searchInput();
+        await searchInput.fill(value);
     }
 
-  
+
+    async validateResultsAreEmpty() {
+        const listItems = this.elements.listItems();
+        const itemCount = await listItems.count();
+        expect(itemCount).toBe(0); 
+    }
 }
