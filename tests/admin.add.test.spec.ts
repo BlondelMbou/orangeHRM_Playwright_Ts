a718ea1747d649447fc1e57b7c1eaae64616712f
BlondelMbou <blondel.mbou@gmail.com>
2024-12-02 10:57:39 +0100

    correction du test  admin.add.test

--- tests/admin.add.test.spec.ts
+++ tests/admin.add.test.spec.ts
@@ -1,18 +1,18 @@
 
 import test, { expect } from "@playwright/test";
 import LoginPage from "../pages/login.page.ts";
-import AdminPage from "../pages/admin.page.ts";
+import AddAdmin from "../pages/admin.page.ts";
 import AddButton from "../pages/admin.add.page.ts";
 
 test.describe("click on admin button on dashboard", () => {
     let loginPage: LoginPage
-    let adminPage: AdminPage
+    let addAdmin: AddAdmin
     let addButton: AddButton
 
 
     test.beforeEach(async ({page}) => {
         loginPage = new LoginPage(page)
-        adminPage = new AdminPage(page) 
+        addAdmin = new AddAdmin(page) 
         addButton = new AddButton(page)
 
         await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
@@ -20,9 +20,9 @@ test.describe("click on admin button on dashboard", () => {
     test("click on admin button on dashboard", async ({page}) => {
         await loginPage.login("Admin", "admin123");
         await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
-        await adminPage.clickAdmin();
+        await addAdmin.clickAdmin();
         await expect(page).toHaveURL("https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewSystemUsers");
-        await addButton.clickAdd();
+        await addAdmin.clickAddButton();
         await expect(page).toHaveURL("https://opensource-demo.orangehrmlive.com/web/index.php/admin/saveSystemUser");
     })
 })
